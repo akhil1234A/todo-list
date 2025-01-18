72f9d203be3eb7bfc3a7ab6c894d53ddfc88f665
Akhil Anwer M <akhilanwarm@gmail.com>
2025-01-18 11:49:11 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import "./index.css";
 import { NewTodoForm } from './NewTodoForm';
-import { TodoList } from './TodoList'; // Ensure the path is correct
+import { TodoList } from './TodoList'; 
 
 export default function App() {
+
   const [todos, setTodos] = useState(()=>{
     const localValue = localStorage.getItem('ITEM')
     if(localValue==null) return []
     return JSON.parse(localValue);
   });
+
    useEffect(()=>{
     localStorage.setItem('ITEM',JSON.stringify(todos))
    },[todos])
+
+
   function addTodo(title) {
     setTodos(currentTodos => [
       ...currentTodos,
@@ -36,7 +40,7 @@ export default function App() {
   return (
     <>
       <NewTodoForm onSubmit={addTodo} />
-      <h1 className="header">Todo List</h1>
+      <h1 className="header">Test List</h1>
       <TodoList 
         todos={todos} 
         onToggleTodo={toggleTodo} 
